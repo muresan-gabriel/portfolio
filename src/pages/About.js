32e42865268031e8a719011160460e7c72514e43
muresan-gabriel <gabiimuresan23@gmail.com>
2022-07-26 15:48:57 +0300

    WIP Alert

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
     <div className="about-container container d-flex flex-column">
-      <h1 className="main-about-heading">I'm Gabriel</h1>
-      <p className="about-paragraph">
+      <div class="alert alert-info page-progress-alert fadein-1" role="alert">
+        This page is currently work in progress. It the meantime you can explore
+        my <Link to="/portfolio">portfolio</Link>.
+      </div>
+      <h1 className="main-about-heading fadein-3">I'm Gabriel</h1>
+      <p className="about-paragraph fadein-4">
         I'm a web developer and a graphic designer.
       </p>
     </div>
