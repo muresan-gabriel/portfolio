efdde582c910cc381b1f3083ece7841f8cf6a07e
muresan-gabriel <gabiimuresan23@gmail.com>
2022-07-29 18:03:39 +0300

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,12 @@ const About = () => {
     <div className="about-container container d-flex flex-column">
       <div class="alert alert-info page-progress-alert fadein-1" role="alert">
         This page is currently work in progress. It the meantime you can explore
-        my <Link to="/portfolio">portfolio</Link>.
+        my <Link to="/portfolio">portfolio</Link>.npm
       </div>
       <h1 className="main-about-heading fadein-3">I'm Gabriel</h1>
       <p className="about-paragraph fadein-4">
         I'm a web developer and a graphic designer.
+        <br />
       </p>
     </div>
   );
